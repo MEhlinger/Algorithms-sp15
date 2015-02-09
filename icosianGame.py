c934cf6e99b35c2d943e05c6d45f6c1f11df0eaf
Marshall Ehlinger <mehlinger333@gmail.com>
2015-02-09 18:04:22 -0500

    Finished icosian game / hamiltonian path algorithm

--- icosianGame.py
+++ icosianGame.py
@@ -47,3 +47,37 @@ def buildIcosian(ring, edges):
 		buildIcosian(ring+5, edges)
 
 	return edges
+
+
+def hamiltonianPath(graph):
+	startNode = graph[0]
+	path = [0]	# Path begins with the start node, 0
+	nextNode(path, graph, startNode, False) # Recursively builds and checks a tree
+	return "Finished."
+
+
+def nextNode(path, graph, goalNeighbors, finishedBool):
+	if not (finishedBool):
+		if (path[len(path)-1] in goalNeighbors) and (len(path) == len(graph)):
+			finishedBool = True
+			printFullPath(path)
+		neighborSet = graph[path[len(path) - 1]]
+		for neighbor in neighborSet:
+			if neighbor not in path:
+				path.append(neighbor) 
+				nextNode(path, graph, goalNeighbors, finishedBool)
+			if neighbor == path[len(path) - 1]:
+				path.pop()
+	
+
+def printFullPath(path):
+	print str(path) + " back to -> " + str(path[0])
+
+
+
+#####################################################
+# The test itself! Blam-o!
+edges = []
+icosian = buildIcosian(5, edges)
+print hamiltonianPath(icosian)
+######################################################
