588473c2a28f26cb56b2c1f58ce64687a2c4a373
Marshall Ehlinger <mehlinger333@gmail.com>
2015-03-11 13:22:06 -0400

    Added insertion sort in C, and updated gitignore to ignore extension-less binaries, as C tends to produce

---
+++ insertionSort.c
@@ -0,0 +1,24 @@
+/* 
+* Insertion Sort
+* by Marshall Ehlinger
+* Written for Spring 2015 Analysis of Algorithms
+*/
+# include <stdio.h>;
+
+main() {
+	int i, j, k, temp;
+	int array[] = {10, 2, 5, 2, 8, 9, 3};
+	int arraySize = sizeof(array) / sizeof(array[0]);
+
+	for (i = 1; i < arraySize; i++) {
+		for (j = i; array[j - 1] > array[j]; j--) {
+			temp = array[j];
+			array[j] = array[j-1];
+			array[j-1] = temp;
+		}
+	}
+
+	for (k = 0 ; k < arraySize ; k++) {
+		printf("%d\n", array[k]);
+	}
+}
